937f5e34ece5a38a92d44bda81b051808390f7ab
bowlerguy66 <mullen.ethan@yahoo.com>
2024-09-09 16:59:04 -0700

    Added command line arguments and update README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
+## Overview ##
 A really simple python program to visualize dependencies with .asmdef files, especially useful for 
 resolving circular dependencies.<br>
+
+## Arguments ##
+Command line arguments are required to point the script to your project directory, optional
+arguments are provided to refine your search.
+
+<root_path>: The root directory where .asmdef files are searched for.
+--ns_whitelist <namespace1,namespace2,...> (optional): A comma-separated list of namespaces to whitelist. All other namespaces will be ignored.
--- dependencyvisualizer.py
+++ dependencyvisualizer.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 import matplotlib.pyplot as plt
 import networkx as nx
+import argparse
 
 def read_json(json_path):
     try:
@@ -15,19 +16,21 @@ def read_json(json_path):
         print(f"Failed to decode json in file {json_path}")
 
 class DependencyVisualizer:
-    def __init__(self, root_path, namespace_blacklist):
+    def __init__(self, root_path, namespace_blacklist=[]):
         self.root_dir = Path(root_path)
         self.asmdef_files = []
         self.graph = nx.DiGraph()
-        self.namespace_blacklist = namespace_blacklist
+        self.namespace_whitelist = namespace_blacklist
 
-    def is_blacklisted(self, namespace):
-        return not any(namespace.startswith(prefix) for prefix in self.namespace_blacklist)
+    def is_whitelisted(self, namespace):
+        if(self.namespace_whitelist is None or len(self.namespace_whitelist) == 0):
+            return True
+        return any(namespace.startswith(prefix) for prefix in self.namespace_whitelist)
 
     def find_asmdef_files(self):
         self.asmdef_files = []
         for file_path in self.root_dir.rglob('*'):
-            if file_path.is_file() and file_path.suffix == ".asmdef":  # Ensure it's a file
+            if file_path.is_file() and file_path.suffix == ".asmdef":
                 self.asmdef_files.append(file_path)
 
     def generate_edges(self):
@@ -39,20 +42,29 @@ class DependencyVisualizer:
 
             name = json["name"]
 
-            if(self.is_blacklisted(name)):
+            if(not self.is_whitelisted(name)):
                 print("Blocked namespace:",name)
                 continue
 
             for reference in json["references"]:
-                if(self.is_blacklisted(reference)):
+                if(not self.is_whitelisted(reference)):
                     continue
                 self.graph.add_edge(json["name"], reference)
 
-vis = DependencyVisualizer("C:/East/Prog/Unity/PackDev", ["Core", "Bootstrapper", "Networking", "PlayerMgmt"])
+parser = argparse.ArgumentParser(description="Visualize .asmdef dependencies")
+parser.add_argument('root_path', type=str, help="The root filepath that .asmdef files will be searched for")
+parser.add_argument('--ns_whitelist', type=str, help="Optional, a list of namespaces that are whitelisted. All other namespaces will be ignored. (SEPARATE VALUES WITH COMMAS AND NO SPACES)")
+args = parser.parse_args()
+
+ns_whitelist = []
+if(args.ns_whitelist is not None):
+    ns_whitelist = args.ns_whitelist.split(",")
+
+vis = DependencyVisualizer(args.root_path, ns_whitelist)
 vis.find_asmdef_files()
 vis.generate_edges()
 
 print(f"Cycles: {list(nx.simple_cycles(vis.graph))}")
 
-nx.draw(vis.graph, with_labels=True, node_size=2000, node_color='lightblue', font_size=10, font_weight='bold', arrows=True)
+nx.draw(vis.graph, nx.circular_layout(vis.graph), with_labels=True, node_size=2000, node_color='lightblue', font_size=10, font_weight='bold', arrows=True)
 plt.show()
